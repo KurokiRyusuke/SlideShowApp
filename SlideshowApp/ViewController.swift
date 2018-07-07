ea0c9e4fb62fb5aedfd8e066047d7a8b497a5790
黒木龍介 <kurokiryuusuke@h58.253.225.10.1017429.vlan.kuins.net>
2018-07-07 16:48:44 +0900

    進むボタンを実装しました。 進むボタンを押すと次の写真に移動します。 また、写真が一巡するともう一度最初の写真に戻ります。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -1,25 +1,44 @@
-//
-//  ViewController.swift
-//  SlideshowApp
-//
-//  Created by 黒木龍介 on 2018/07/07.
-//  Copyright © 2018年 Ryusuke.Kuroki. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+//UIImageVIewの接続-------------------------------------------------------------------------
+   @IBOutlet weak var Picture: UIImageView!
+
+    //写真の番号を指定する変数Numberを宣言
+    var Number: Int = 1
+    
+    
+//viewDisLoad------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        //画像ファイルを読み込む
+        var image = UIImage(named: "1.jpg")
+        Picture.image = image
     }
-
+    
+    
+//MemoryWarning----------------------------------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+//「進む」ボタン------------------------------------------------------------------------------
+    @IBAction func Go(_ sender: Any) {
+        if Number < 20 {
+        Number += 1
+        var image = UIImage(named: "\(Number).jpg")
+        Picture.image = image
+        } else {
+        Number = 1
+        var image = UIImage(named: "1.jpg")
+        Picture.image = image
+        }
+    }
 
 }
 
