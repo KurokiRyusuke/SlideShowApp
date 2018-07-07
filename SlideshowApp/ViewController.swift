ed7a3be4998d8a1defc5f17035ae577425e9d0b9
黒木龍介 <kurokiryuusuke@h58.253.225.10.1017429.vlan.kuins.net>
2018-07-07 17:11:18 +0900

    戻るボタンを実装しました。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -33,19 +33,39 @@ class ViewController: UIViewController {
     
     
 //「進む」ボタン------------------------------------------------------------------------------
+//ボタンを押すたびに、番号が１つ上の写真に移動します
     @IBAction func Go(_ sender: Any) {
         if Number < a {
+        //番号がa以下であれば次の番号に移動します
         Number += 1
         var image = UIImage(named: "\(Number).jpg")
         Picture.image = image
         } else {
+        //番号が枚数を超えた時、1番の写真に戻ります
         Number = 1
         var image = UIImage(named: "1.jpg")
         Picture.image = image
         }
     }
 //------------------------------------------------------------------------------------------
-    
+
+
+//「戻るボタン」-------------------------------------------------------------------------------
+//ボタンを押すために、番号が１つ下の写真に移動します
+    @IBAction func Back(_ sender: Any) {
+        if Number > 1 {
+            //番号が1よりか大きい場合、１つ下の番号の写真に移動します
+            Number -= 1
+            var image = UIImage(named: "\(Number).jpg")
+            Picture.image = image
+        } else {
+            //番号が1となった場合、写真番号がaである写真に移動します
+            Number = 20
+            var image = UIImage(named: "\(a).jpg")
+            Picture.image = image
+        }
+    }
+//-------------------------------------------------------------------------------------------
     
 }
 
